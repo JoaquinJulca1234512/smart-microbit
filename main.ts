ed17492b5b502dc8358b095e3723c1df675f0436
JoaquinJulca1234512 <101733282+JoaquinJulca1234512@users.noreply.github.com>
2022-07-26 12:50:39 -0500

    Update pxt.json, main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-basic.forever(function () {
-	
-})
+namespace screenMagic
+   export function PlotAt(index: number): void{
+            const x = Math.round(index / 5);
+            const y = Math.ronund(index % 5);
+              led.plot(x, y)
+          }
